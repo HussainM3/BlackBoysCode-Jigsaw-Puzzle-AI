514ae37a1c576025eb477f52d4e8c7b620b2f9bb
Hussain Muhammed <hussainmohd2003@outlook.com>
2024-03-28 03:47:52 +0000

    updated description for each part

--- jigsaw-puzzle_solver_full_code.py
+++ jigsaw-puzzle_solver_full_code.py
@@ -1,13 +1,8 @@
 """
 This is the main file with all functions for the jigsaw puzzle solver. 
 
-The original author of this file is Maxim Terleev. The file was retrieved from the following GitHub repository:
+The original author of this application is Maxim Terleev. The source files can be seen in the following GitHub repository:
 https://github.com/MaximTerleev/Jigsaw-Puzzle-AI/blob/main/jigsaw-puzzle-solver.py
-
-The solver contains many functions and can be split into 3 main sections:
-1. Image Processing
-2. Matching
-3. Assembling
 
 """
 
