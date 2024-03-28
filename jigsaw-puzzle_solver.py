2684f64ef78e0acd339935ceb09940241ef2c455
Hussain Muhammed <hussainmohd2003@outlook.com>
2024-03-28 00:26:59 +0000

    setup main jigsaw solver python file

---
+++ jigsaw-puzzle_solver.py
@@ -0,0 +1,13 @@
+"""
+This is the main file with all functions for the jigsaw puzzle solver. 
+
+The original author of this file is Maxim Terleev. The file was retrieved from the following GitHub repository:
+https://github.com/MaximTerleev/Jigsaw-Puzzle-AI/blob/main/jigsaw-puzzle-solver.py
+
+The solver contains many functions and can be split into 3 main sections:
+1. Image Processing
+2. Matching
+3. Assembling
+
+"""
+
