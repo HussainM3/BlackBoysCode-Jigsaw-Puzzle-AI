f4eec309851e8a306885484102470c60f2ba6fba
Hussain Muhammed <hussainmohd2003@outlook.com>
2024-03-28 00:56:34 +0000

    added functions and code related to Matching section

--- jigsaw-puzzle_solver.py
+++ jigsaw-puzzle_solver.py
@@ -20,6 +20,8 @@ from fastdtw import fastdtw
 import numpy as np
 import cv2
 
+
+
 """# Image processing"""
 
 # @title Functions
@@ -78,3 +80,153 @@ for i in range(len(contours)):
 
 showlist(tiles)
 
+
+
+"""# Matching"""
+
+# Rescale tiles to assembly format
+canvas_tiles = []
+for i in range(len(tiles)):
+  canvas_tile = np.zeros((1400,1400,4), 'uint8')
+  canvas_tile[550:850, 550:850] = tiles[i].copy()
+  canvas_tiles.append(canvas_tile)
+
+# @title Functions
+
+def getColors(image, subcontour):
+  subcontour = np.flip(subcontour)
+
+  colors = []
+  for n in range(len(subcontour)-3):
+    (y,x) = subcontour[n]
+    (y1,x1) = subcontour[n+3]
+    h, w = y1 - y, x1 - x
+    colors.append(image[y-w, x+h, :3] + image[y+w, x-h, :3])
+
+  colors = np.array(colors, 'uint8').reshape(-1,1,3)
+  colors = cv2.cvtColor(colors, cv2.COLOR_RGB2HSV)
+  
+  return colors.reshape(-1,3)
+
+def putOnAnvil(arr_img, point, angle, center=(700,700)):
+  img = Image.fromarray(arr_img)
+  img = ImageChops.offset(img, center[1] - point[1], center[0] - point[0])
+  img = img.rotate(angle)
+
+  return np.array(img)
+
+def rotatePoint(point, angle, center=(700,700)):
+  dy, dx = center[0]-point[0], point[1]-center[1]
+  distance = np.sqrt(np.square(point[0]-center[0]) + np.square(point[1]-center[1]))
+  if dx==0: dx = 1
+  base = 90*(1-np.sign(dx)) + np.degrees(np.arctan(dy/dx))
+  
+  y = round(center[0] - distance * np.sin(np.pi * (base + angle)/180))
+  x = round(center[1] + distance * np.cos(np.pi * (base + angle)/180))
+
+  return (y,x)
+
+def reScale(point, position, center=(150,150)):
+  cy, cx, angle = position
+  if angle!=0: (y, x) = rotatePoint(point, angle, center)
+  else: (y, x) = point
+
+  return (y + cy - center[0], x + cx - center[1])
+
+# @title Matching algorithm
+def matchTiles(A, B):
+
+  LENGTH = 160
+  PRECISION = 8
+  STEP_A = 20
+  STEP_B = 7
+  MAX_FORM = 0.015
+  MAX_COLOR = 8000
+  MAX_PIXEL = 0.03
+  MAX_FIT = 0.77
+
+  CENTER = round(LENGTH/2)
+
+  tileA, tileB = tiles[A], tiles[B]
+  cntA, _ = cv2.findContours(tileA[:,:,3], 0, 1)
+  cntB, _ = cv2.findContours(tileB[:,:,3], 0, 1)
+  cntA, cntB = cntA[0].reshape(-1,2), cntB[0].reshape(-1,2)
+  sumLen = cntA.shape[0] + cntB.shape[0]
+
+  # Contour matching
+  form_matches = []
+  for i in range(0, cntA.shape[0], STEP_A):
+
+    # subcontour A and its type
+    subcA = np.roll(cntA, -i, 0)[:LENGTH]
+    pointA = tuple(np.flip(subcA[CENTER]))
+    cA, (hA,wA), aA = cv2.minAreaRect(subcA)
+    typepointA = np.int0(np.flip(subcA[0] + subcA[-1] - cA))
+    typeA = tileA[:,:,3][tuple(typepointA)]
+    a = cv2.drawContours(np.zeros((300,300),'uint8'), subcA.reshape(-1,1,2), -1, 255, 1)
+
+    # loop through match subcontours
+    for j in range(0, cntB.shape[0], STEP_B):
+      
+      # subcontour B and its type
+      subcB = np.roll(cntB, -j, 0)[:LENGTH]
+      pointB = tuple(np.flip(subcB[CENTER]))
+      cB, (hB,wB), aB = cv2.minAreaRect(subcB)
+      typepointB = np.int0(np.flip(subcB[0] + subcB[-1] - cB))
+      typeB = tileB[:,:,3][tuple(typepointB)]
+
+      # record good form matches
+      if typeB != typeA:
+        if ((abs(hA-hB) < PRECISION) & (abs(wA-wB) < PRECISION)) or ((abs(hA-wB) < PRECISION) & (abs(wA-hB) < PRECISION)):
+          b = cv2.drawContours(np.zeros((300,300),'uint8'), subcB.reshape(-1,1,2), -1, 255, 1)
+          fmatch = cv2.matchShapes(a,b,1,0)
+          if fmatch < MAX_FORM: 
+            colinear = True if np.sign(hA-wA) == np.sign(hB-wB) else False
+            if colinear:
+              codirect = True if (np.sign(typepointA - np.flip(cA)) ==  np.sign(typepointB - np.flip(cB))).all() else False
+            else:
+              c = np.concatenate([np.sign(typepointA - np.flip(cA)), np.sign(typepointB - np.flip(cB))])
+              codirect = True if (abs(np.sum(c[:3])) + abs(np.sum(c[-3:]))) == 4 else False
+            if not colinear: aB = aB + 90
+            if not codirect: aB = aB + 180  
+            form_matches.append([(i, j), pointA, pointB, round(aB-aA,4), round(fmatch,4)])
+ 
+  # Color matching
+  color_matches = []
+  for n in range(len(form_matches)):
+    (i, j), pointA, pointB, angle, fmatch = form_matches[n]
+    subcA = np.roll(cntA, -i, 0)[:LENGTH] 
+    subcB = np.roll(cntB, -j, 0)[:LENGTH]
+    colorsA = getColors(tileA, subcA)
+    colorsB = getColors(tileB, subcB)
+    cmatch = fastdtw(colorsA, np.flip(colorsB, axis=0))[0]
+    if cmatch < MAX_COLOR: 
+      color_matches.append([(i, j), pointA, pointB, angle, fmatch, round(cmatch)])
+
+  # Pre-fitting
+  fit_matches = []
+  for n in range(len(color_matches)):
+    (i, j), pointA, pointB, angle, fmatch, cmatch = color_matches[n]
+    a = putOnAnvil(canvas_tiles[A][:,:,3], reScale(pointA, [700,700,0]), 0)
+    b = putOnAnvil(canvas_tiles[B][:,:,3], reScale(pointB, [700,700,0]), angle)
+    loss = 1 - (np.sum((a+b)>0) / (np.sum(a>0) + np.sum(b>0)))
+    contours, _ = cv2.findContours((a+b), 0, 1)
+    fit = contours[0].shape[0] / sumLen
+    if (loss < MAX_PIXEL) & (fit < MAX_FIT): 
+      fit_matches.append([(A, B), (i, j), pointA, pointB, angle, fmatch, cmatch, round(loss+fit,4), 0])
+
+  fit_matches.sort(key=lambda n: n[-1])
+
+  return fit_matches
+
+# Calculate all possible matches
+matches = []
+for a in range(len(tiles)-1):
+  for b in range(a+1,len(tiles)):
+    matches.extend(matchTiles(a,b))
+
+# Flip and sort
+for n in range(len(matches)):
+  pair, ij, pointa, pointb, angle, fmatch, cmatch, fit, lock = matches[n]
+  matches.extend([[(pair[1],pair[0]), ij, pointb, pointa, -angle, fmatch, cmatch, fit, lock]])
+matches.sort(key=lambda m: (m[0], m[-2]))
